f3167a83985391847ea7e57daac10da0ce1bff79
lindqvistfrida <44609089+lindqvistfrida@users.noreply.github.com>
2019-01-22 13:25:18 +0100

    Update Drowsi_test.m

--- Drowsi_test.m
+++ Drowsi_test.m
@@ -1,12 +1,21 @@
 %% Drowsi WP4
 
+%% FP1
+
+fp1_1 = edf2struct('FP1 2.edf');
+% fp1_2 = edf2struct('FP1 2.edf');
+% fp1_3 = edf2struct('FP1 3.edf');
+
+
+%% Create new struct
+
+WP4_1_1 = fp1_1;
+
+
 %%
 
-fp1_3 = edf2struct('fp1 - 3.edf');
-
-%%
-
-Marker = fp1_3.data8.Marker;
+Marker = fp1_1.data8.Marker;
+figure(1)
 plot(Marker)
 
 %% Time 
@@ -16,7 +25,7 @@ start_index = index_nonzero(1);
 
 end_index = index_nonzero(end);
 
-time = fp1_3.data8.time;
+time = fp1_1.data8.time;
 
 %After this amount of time the experiment started
 start_time = time(start_index);
@@ -24,10 +33,10 @@ start_time = time(start_index);
 %After this amount of time the experiment ended
 end_time = time(end_index);
 
-%%
+%% ECG
 
-ECG = fp1_3.data256.ECG;
-ECG_time = fp1_3.data256.time;
+ECG = fp1_1.data256.ECG;
+ECG_time = fp1_1.data256.time;
 
 ECG_start_index = round(start_time)*256;
 ECG_end_index = round(end_time)*256;
@@ -38,13 +47,71 @@ ECG_end = ECG_time(ECG_end_index); % stämmer
 %Extract ECG of interest 
 ECG = ECG(ECG_start_index:ECG_end_index);
 
+%% EOG 
+
+EOG = fp1_1.data512.EOG_vv;
+EOG_time = fp1_1.data512.time;
+
+EOG_start_index = round(start_time)*512;
+EOG_end_index = round(end_time)*512;
+
+EOG_start = EOG_time(EOG_start_index); % stämmer
+EOG_end = EOG_time(EOG_end_index); % stämmer
+
+%Extract ECG of interest 
+EOG = EOG(EOG_start_index:EOG_end_index);
+
+
+%% New time vector
+
+%TimeSync vector for data1
+%timeSync = fp1_1.data1-start_time;
+
+%TimeSync vector for data8
+%timeSync = time-start_time;
+
+%TimeSync vector for data 256
+%timeSync = fp1_1.data256.time-ECG_start;
+
+%TimeSync vector for data 512
+timeSync = fp1_1.data512.time-EOG_start;
+
 %%
 
-% New time vector
+length_exp = (end_time - start_time)/60; %in minutes
 
-timeSync = zeros(length(time)+start_index,1);
+figure(2)
+plot(ECG)
 
-timeSync(start_index:end-1) = time;
+%% Adding timeSync to struct 
+
+%WP4_1_1.data512.timeSync = timeSync;
+
+%% Adding KSS
+
+KSS = zeros(52640,1);
+
+%Time point 45 min 
+index_45 = find(timeSync == 45*60);
+
+% Set values of KSS for driving to 4
+KSS(start_index:index_45) = 4;
+
+% Set values that are 0 to NaN
+temp = find(~KSS);
+KSS(temp) = NaN;
+
+%% Adding KSS to struct
+
+%WP4_1_1.data8.KSS = KSS;
+
+%%KSS_table = readtable('KSS Körning.xls')
+
+%% Save struct
+
+%PHYS = WP4_1_1;
+
+%save('WP4_1_1.mat', 'PHYS')
 
 
 
